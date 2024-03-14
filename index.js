2e8d9426230f6440bac8fec35ff0ed40cd77065d
Guilherme B <dogui0202@gmail.com>
2024-03-14 18:02:32 -0300

    tá quase funcionando, marginRight me surpreendeu com a funcionalidade mas ainda não consegui o efeito desejado

---
+++ index.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    $('#Expandir').click(function() {
+        $('.TecladoNumerico').animate({
+            marginLeft: '+=380px',
+            opacity: 0
+        },2000);
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
