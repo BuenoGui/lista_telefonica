083c6bda78d845a8becd4cac6593feb4e6a2e39a
Guilherme B <dogui0202@gmail.com>
2024-03-15 06:15:47 -0300

    Lógica de adicionar linhas a Tabela feita, pensando em tirar o Tfoot pois me parece irelevante agora, ainda falta ajustar o teclado númerico com lógica e resolver o bug de ter dois contatos com o mesmo número

--- src/index.js
+++ src/index.js
@@ -5,7 +5,52 @@ $(document).ready(function() {
         }, 1300);
         $('.TecladoNumerico').css({
             display: 'flex'
-        })
-        
+        })    
     });
+
+
+    const contatos = [];
+    const nums = [];
+
+
+    let contador = 1;
+    let linhas = "";
+
+
+
+    $('form').submit(function(adicionar) {
+        adicionar.preventDefault();
+
+        adicionarLinha();
+        atualizaTabela();
+    })
+    
+    function adicionarLinha() {
+        const nomeContato = document.getElementById('nomeContato');
+        const numeroContato = document.getElementById('numeroContato');
+
+        if(contatos.includes(numeroContato.value)) {
+            alert('Você não pode ter dois contatos com o mesmo numero');
+        } else {
+            contatos.push(nomeContato.value);
+            nums.push(numeroContato);
+
+            let linha = '<tr>';
+            linha += `<td> ${nomeContato.value} </td>`;
+            linha += `<td> ${numeroContato.value} </td>`;
+            linha += `<td> ${contador} <td>`
+            linha += '</tr>';
+
+            linhas += linha;
+            contador++;
+        }
+        nomeContato.value = "";
+        numeroContato.value = "";
+    }
+
+    function atualizaTabela() {
+        const corpoTabela = document.querySelector('tbody');
+        corpoTabela.innerHTML = linhas;
+    }
+
 });
